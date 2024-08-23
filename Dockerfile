e8863109a439aa32f5e490d16b3d97ac7b6114bd
Kanika Naik <kanikalnaik@gmail.com>
2024-08-23 13:45:40 +0530

    git puch

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,8 @@
 FROM python:3.13-rc-bookworm
 COPY . . 
+COPY requirements.txt .
+
+RUN pip install --no-cache-dir -r requirements.txt
+
+EXPOSE 5000
 CMD ["python","./hello.py"]
--- hello.py
+++ hello.py
@@ -1,1 +1,24 @@
-print("hello world, this is kanika naik")
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+
+
+@app.route("/api/greet", methods=["GET"])
+def greet():
+    name = request.args.get("name", "Guest")
+    return jsonify({"message": f"Hello, {name}!"})
+
+
+@app.route("/api/echo", methods=["POST"])
+def echo():
+    data = request.json
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
